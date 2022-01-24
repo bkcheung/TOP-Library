e4d7d4be09ea7ac60812608ee7e4b099e379355e
bkcheung <b.cheung@hotmail.com>
2022-01-23 16:55:24 -0800

    Update library table output

--- library.js
+++ library.js
@@ -1,6 +1,4 @@
 let myLibrary = [];
-const library = document.getElementById("library");
-
 
 function Book(title, author, pages, read){
     this.title = title
@@ -15,28 +13,65 @@ function Book(title, author, pages, read){
 
 function addBookToLibrary(title, author, pages, read){
     const newBook = new Book(title, author, pages, read);
+
+    const newRow = document.createElement("tr");
+
+    const newTitle = document.createElement("td");
+    newTitle.innerHTML = title;
+    newRow.append(newTitle);
+
+    const newAuthor = document.createElement("td");
+    newAuthor.innerHTML = author;
+    newRow.append(newAuthor);
+
+    const newPages = document.createElement("td");
+    newPages.innerHTML = pages;
+    newRow.append(newPages);
+
+    const readStatus = document.createElement("td");
+    const bookRead = read ? 'Read':'Not read yet';
+    readStatus.innerHTML = bookRead;
+    newRow.append(readStatus);
+
+    document.getElementById("libraryTable").appendChild(newRow);
+
     myLibrary.push(newBook);
+
 }
 
 function displayBook () {
-    let displayInfo = [];
-    myLibrary.forEach(element => {
-        const info = element.info();
-        displayInfo.push(info);
-    });
-    console.log(displayInfo);
-    library.textContent = displayInfo;
+    const library = document.getElementById("libraryTable");
+
+    for(i=0; i < myLibrary.length; i++){
+        const info = myLibrary[i].info();
+        const row = library.insertRow(i);
+        row.innerHTML = info;
+    }
+
+    
+    
 }
 
 function openForm(){
     document.getElementById("newBookForm").style.display = "block";
 }
 
-function closeForm(){
+function submitForm(){
+    const title = document.getElementById("bookTitle").value;
+    const author = document.getElementById("bookAuthor").value;
+    const pages = document.getElementById("bookPages").value;
+    const read = document.getElementById("bookRead").checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    // displayBook();
+
     document.getElementById("newBookForm").style.display = "none";
 }
 
-const book1 = addBookToLibrary('Name of the Wind', 'Patrict Rothfuss', 662, true);
+const library = document.getElementById("libraryTable");
+
+const book1 = addBookToLibrary('Name of the Wind', 'Patrick Rothfuss', 662, true);
 const book2 = addBookToLibrary('The Way of Kings', 'Brandon Sanderson', 1007, true);
 
-displayBook();
+// displayBook();
